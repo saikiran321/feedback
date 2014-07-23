74718db8eec5e63b06add34bc90aafa28be4af55
Dharani Manne <dharani.manne@gmail.com>
2014-07-23 18:57:33 +0530

    updated post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
-  belongs_to :user
-  has_many :comments
+  belongs_to :user, inverse_of: :posts, dependent: :destroy
+  has_many :comments, inverse_of: :posts, dependent: :destroy
   has_and_belongs_to_many :tags
 
   validates :title, presence: true
