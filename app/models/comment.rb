a3c027b54ca30c4180b0a3eb16a4123b69ec64e5
Dharani Manne <dharani.manne@gmail.com>
2014-07-24 00:26:37 +0530

    changed models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
 
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, inverse_of: :comments, dependent: :destroy
+  belongs_to :post, inverse_of: :comments, dependent: :destroy
 
   validates :content, presence: true
+  validates :user_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ActiveRecord::Base
 
   validates :title, presence: true
   validates :content, presence: true
+  validates :user_id, presence: true
 end
