beb76177dbb52298cc082c7eae7bb45cdf54beff
Dharani Manne <dharani.manne@gmail.com>
2014-07-28 10:20:41 +0530

    minor bug fix

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,12 @@
 class TagsController < ApplicationController
 
   def index
-    @posts = Post.where("solved=?", false).reverse_order
+    @posts = Post.all.reverse_order
   end
 
   def show
     @tag = Tag.find(params[:id])
-    @posts = Tag.includes(:posts).find(@tag.id).posts.where("solved=?", false)
+    @posts = Tag.includes(:posts).find(@tag.id).posts
   end
 
 end
