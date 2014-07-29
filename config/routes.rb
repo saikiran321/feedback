87424db9d0fa8fcb5e93b1631082ac67004d5824
Dharani Manne <dharani.manne@gmail.com>
2014-07-29 22:35:47 +0530

    slight frontend changes

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  #
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
