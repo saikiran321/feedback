beafc3d3674550cf2efebb69b85954ba0765794a
Dharani Manne <dharani.manne@gmail.com>
2014-07-29 18:54:54 +0530

    bug fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def tag_it(comment)
    comment.content.scan(/\+[a-zA-Z]{2}[0-9]{2}[a-zA-Z]{1}[0-9]{3}/).each do |roll|
-     @user = User.where("username=?", roll.upcase).take
+     @user = User.where("username=?", roll.strip[1..8]upcase).take
      comment.content.sub!(roll, "<a href='/users/#{@user.id}'>#{@user.fullname.capitalize}</a>")
    end
   end
