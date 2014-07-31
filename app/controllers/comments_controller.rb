e0b9331710addef695f5c60fa42b04d36e7e54fa
Dharani Manne <dharani.manne@gmail.com>
2014-07-31 22:09:33 +0530

    both adding and deleting comments in ajax done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,50 +3,56 @@ class CommentsController < ApplicationController
   include ApplicationHelper
 
   def create
+    @post = Post.find(params[:post_id])
     @comment = Comment.new
     @comment.content = params[:comment][:content]
     @comment.user_id = current_user.id
-    @comment.post_id = Integer(params[:post_id])
+    @comment.post_id = @post.id
     tag_it @comment
     if @comment.save
       if current_user.id != User.find(Post.find(params[:post_id]).user_id).id
         @notif = Notification.new
         @notif.user_id = User.find(Post.find(params[:post_id]).user_id).id
-        @notif.post_id = Integer(params[:post_id])
+        @notif.post_id = @post.id
         @notif.notif_user = current_user.id
         @notif.action = 'commented on your post'
         @notif.save
       end
-      @users = User.find(Comment.where("post_id = ?", Post.find(params[:post_id])).uniq.pluck(:user_id))
+      @users = User.find(Comment.where("post_id = ?", @post.id).uniq.pluck(:user_id))
       @users.each do |user|
-        if  current_user.id != user.id && user.id != User.find(Post.find(params[:post_id]).user_id).id
+        if  current_user.id != user.id && user.id != User.find(@post.user_id).id
           @notif = Notification.new
           @notif.user_id = user.id
-          @notif.post_id = Integer(params[:post_id])
+          @notif.post_id = @post.id
           @notif.notif_user = current_user.id
           @notif.action = 'commented on a post that you have commented on'
           @notif.save
         end
       end
-      flash[:success]="Commented successfully"
-      redirect_to :back
+      respond_to do |format|
+        format.html {redirect_to @comment}
+        format.js
+      end
     else
       render 'new'
     end
   end
 
   def destroy
-    if current_user.id != User.find(Comment.find(params[:id]).user_id).id
+    @comment = Comment.find(params[:id])
+    if current_user.id != User.find(@comment.user_id).id
       @notif = Notification.new
-      @notif.user_id = User.find(Comment.find(params[:id]).user_id).id
-      @notif.post_id = Post.find(Comment.find(params[:id]).post_id).id
+      @notif.user_id = User.find(@comment.user_id).id
+      @notif.post_id = Post.find(@comment.post_id).id
       @notif.notif_user = current_user.id
       @notif.action = 'deleted your comment on a post'
       @notif.save
     end
-    Comment.find(params[:id]).destroy
-    flash[:success]="Successfully deleted the comment"
-    redirect_to :back
+    @post = Post.find(@comment.post_id)
+    respond_to do |format|
+      format.html {redirect_to @comment}
+      format.js
+    end
   end
 
 end
