54dc068dda02c6bf6b0c3bb81bd1c66b4c8ec575
Dharani Manne <dharani.manne@gmail.com>
2014-08-02 16:04:56 +0530

    added non-student junta to users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,43 +19,53 @@ class SessionsController < ApplicationController
     #    end
 
     if username.match(/[a-zA-Z]{2}[0-9]{2}[a-zA-Z]{1}[0-9]{3}/)
-    ldap = Net::LDAP.new :host => "ldap.iitm.ac.in",
-      :port => 389,
-      :auth => {
-        :method => :simple,
-        :username => "cn=students,ou=bind,dc=ldap,dc=iitm,dc=ac,dc=in",
-        :password => "rE11Bg_oO~iC"
-      }
+      ldap = Net::LDAP.new :host => "ldap.iitm.ac.in",
+        :port => 389,
+        :auth => {
+          :method => :simple,
+          :username => "cn=students,ou=bind,dc=ldap,dc=iitm,dc=ac,dc=in",
+          :password => "rE11Bg_oO~iC"
+        }
 
-      if ldap.bind
-        filter = Net::LDAP::Filter.eq("uid", username)
-        treebase = "dc=ldap, dc=iitm, dc=ac, dc=in"
+        if ldap.bind
+          filter = Net::LDAP::Filter.eq("uid", username)
+          treebase = "dc=ldap, dc=iitm, dc=ac, dc=in"
 
-        ldap.search(:base => treebase, :filter => filter) do |entry|
-          puts "DN: #{entry.dn}"
-          if entry.dn
-            ldap1 = Net::LDAP.new
-            ldap1.host = "ldap.iitm.ac.in"
-            ldap1.port = 389
-            ldap1.auth entry.dn, password
-            if ldap1.bind
-              @user = User.find_by(username: params[:session][:roll])
-              if @user
-                sign_in @user
-                redirect_to root_path
-                flash[:success] = "Welcome, #{@user[:fullname]}"
+          ldap.search(:base => treebase, :filter => filter) do |entry|
+            puts "DN: #{entry.dn}"
+            if entry.dn
+              ldap1 = Net::LDAP.new
+              ldap1.host = "ldap.iitm.ac.in"
+              ldap1.port = 389
+              ldap1.auth entry.dn, password
+              if ldap1.bind
+                @user = User.find_by(username: params[:session][:roll])
+                if @user
+                  sign_in @user
+                  redirect_to root_path
+                  flash[:success] = "Welcome, #{@user[:fullname]}"
+                else
+                  redirect_to signin_path
+                  flash[:error] = "Sorry, such a user does not exist in our database."
+                end
               else
-                redirect_to signin_path
-                flash[:error] = "Sorry, such a user does not exist in our database."
+                redirect_to signin_path, :flash => {:error => "Invalid username or password"}
               end
-            else
-              redirect_to signin_path, :flash => {:error => "Invalid username or password"}
             end
           end
         end
-      end
-  end
+    end
   else
+    @user = User.find_by(username: username)
+    if @user.usertype==password
+      sign_in @user
+      redirect_to root_path
+      flash[:success] = "Welcome, #{@user[:fullname]}"
+    else
+      redirect_to signin_path
+      flash[:error] = "Sorry, such a user does not exist in our database."
+    end
+
   end
 
 
