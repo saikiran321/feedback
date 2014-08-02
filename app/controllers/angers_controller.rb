a6f6da11c81f1c2896991898741a6c9c7f94cb9d
Dharani Manne <dharani.manne@gmail.com>
2014-08-02 15:37:57 +0530

    angry level with average level done

--- app/controllers/angers_controller.rb
+++ app/controllers/angers_controller.rb
@@ -1,5 +1,7 @@
 class AngersController < ApplicationController
 
+  include AngersHelper
+
   def create
     @post = Post.find(params[:post_id])
     @anger = Anger.new
@@ -7,6 +9,9 @@ class AngersController < ApplicationController
     @anger.post = @post
     @anger.user = current_user
     @anger.save
+    len = Anger.where("post_id=?", @post.id).length
+    new_level = ((@post.avg_anger*(len-1))+@anger.level)/len
+    @post.update_attributes(avg_anger: new_level)
     respond_to do |format|
       format.html{ redirect_to @anger }
       format.js
@@ -17,6 +22,13 @@ class AngersController < ApplicationController
     @anger = Anger.find(params[:id])
     @post = @anger.post
     @anger.update_attributes(level: params[:i])
+    new_level = 0
+    @angers = Anger.where("post_id=?", @post.id)
+    @angers.each do |anger|
+      new_level = new_level + anger.level
+    end
+    new_level = new_level/@angers.length
+    @post.update_attributes(avg_anger: new_level)
     respond_to do |format|
       format.html {redirect_to @anger}
       format.js
