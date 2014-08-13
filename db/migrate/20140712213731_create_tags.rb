447dddde02f250fb1eb03666bc882e3cf82b3863
Dharani Manne <dharani.manne@gmail.com>
2014-08-13 19:08:31 +0530

    changed migrations

--- db/migrate/20140712213731_create_tags.rb
+++ db/migrate/20140712213731_create_tags.rb
@@ -2,6 +2,7 @@ class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
       t.string :name, null: false, limit: 45
+      t.text :description
 
       t.timestamps
     end
