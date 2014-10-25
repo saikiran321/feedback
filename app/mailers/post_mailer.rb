cd944969514dac6b39653eb0e3093fb3b411b69f
chiranjeevi <chiru6753@gmail.com>
2014-10-25 17:10:32 +0530

    Update PostMailer view

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,7 +1,10 @@
 class PostMailer < ActionMailer::Base
-	default from: "ch12b034@smail.iitm.ac.in"
+  default from: "ch12b034@smail.iitm.ac.in"
   
-	def post_notify(recipient)
-		mail(to:recipient+"@smail.iitm.ac.in",subject:"Welcome to feedback forum")
-	end
+  def post_notify(recipient,post_title)
+    @recipient = recipient
+    @recipient_name = User.find_by(username:recipient).fullname
+    @post_title = post_title
+    mail(to:@recipient+"@smail.iitm.ac.in",subject:"Tagged in post '#{post_title}'")
+  end
 end
